7c91cadc5d4dc8fffdf7b7b66bc5f1c7d70a1209
Dovile Brazinske <dovilebrazinske@gmail.com>
2022-07-25 15:20:32 +0200

    feat: added slider in order page for purchase year

--- src/pages/order-page/index.jsx
+++ src/pages/order-page/index.jsx
@@ -12,6 +12,7 @@ import {
   Radio,
   Divider,
   Checkbox,
+  Slider,
 } from '@mui/material';
 import CheckboxGroup from '../../components/checkbox-group';
 
@@ -75,16 +76,17 @@ const services = [
 ];
 
 const OrderPage = () => {
-  const [fullname, setFullname] = React.useState('');
+  const [device, setDevice] = React.useState(devices[0].value);
   const [manufacturer, setManufacturer] = React.useState('Samsung');
   const [manufacturerCode, setManufacturerCode] = React.useState('RB34T600FSA/EF');
+  const [purchaseYearRange, setPurchaseYear] = React.useState([2014]);
+  const [service, setService] = React.useState(null);
+  const [fullname, setFullname] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [phoneNumber, setPhoneNumber] = React.useState('');
   const [message, setMessage] = React.useState('');
-  const [device, setDevice] = React.useState(devices[0].value);
-  const [service, setService] = React.useState(null);
-  const [subscribtion, setSubscribe] = React.useState(true);
   const [selectedTime, setSelectedTime] = React.useState([]);
+  const [agreement, setAgreement] = React.useState(true);
 
   return (
     <Box sx={{ pt: 7 }}>
@@ -140,6 +142,17 @@ const OrderPage = () => {
             onChange={(event) => setManufacturerCode(event.target.value)}
             value={manufacturerCode}
           />
+          <FormControl sx={{ width: '100%' }}>
+            <FormLabel>Pirkimo metai</FormLabel>
+            <Slider
+              value={purchaseYearRange}
+              min={1990}
+              max={2022}
+              onChange={(_, newPurchaseDate) => setPurchaseYear(newPurchaseDate)}
+              valueLabelDisplay="on"
+              sx={{ mt: 2 }}
+            />
+          </FormControl>
 
           <FormControl sx={{ width: '100%' }}>
             <FormLabel>Pasirinkite paslaugą</FormLabel>
@@ -191,20 +204,21 @@ const OrderPage = () => {
             value={message}
             fullWidth
           />
-
-          <CheckboxGroup
-            label="Pasirinkite Jums tinkamiausius laikus "
-            options={time}
-            value={selectedTime}
-            onChange={(_, newColors) => setSelectedTime(newColors)}
-          />
+          <FormControl sx={{ width: '100%' }}>
+            <CheckboxGroup
+              label="Pasirinkite Jums tinkamiausius laikus "
+              options={time}
+              value={selectedTime}
+              onChange={(_, newColors) => setSelectedTime(newColors)}
+            />
+          </FormControl>
 
           <Box sx={{ alignSelf: 'flex-start' }}>
             <FormControlLabel
               control={(
                 <Checkbox
-                  checked={subscribtion}
-                  onChange={(_, newSubsribtion) => setSubscribe(newSubsribtion)}
+                  checked={agreement}
+                  onChange={(_, newSubsribtion) => setAgreement(newSubsribtion)}
                 />
                             )}
               label="Susipažinau ir sutinku su taisyklėmis ir sąlygomis"
